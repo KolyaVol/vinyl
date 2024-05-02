e60f45de15b694df347bd2dd21242b70c55484e1
VolNa <grizli887@mail.ru>
2024-05-02 19:42:16 +0300

    fix: remove google auth wrong method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -37,10 +37,6 @@ export class UsersController {
     );
   }
 
-  @Get()
-  @UseGuards(AuthGuard('google'))
-  async googleAuth(@Request() req: Req) {}
-
   @Get('/auth/google/callback')
   @UseGuards(AuthGuard('google'))
   googleAuthRedirect(@Request() req: Req) {
